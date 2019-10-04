d024451442f105a47992918117cdbb9b6124fce0
priguis <priihalves_@hotmail.com>
2019-10-04 14:04:45 -0300

    adicionando um botão

--- js/script.js
+++ js/script.js
@@ -1,102 +1,116 @@
-// criando valores para manipula-los
-
-
+// colocando valores em em novas variáveis
 const botaoAdd = document.getElementById("ativaBotao");
 
 const lista = document.getElementById("lista");
 
 const erro = document.querySelector(".meu-erro");
 
-const btnFeito = document.querySelector(".bnt1");
+const btnFeito = document.querySelector (".bnt1");
 
-const btnExcluir = document.querySelector(".bnt2");
+const btnExcluir = document.querySelector (".bnt2");
 
-// Adicionando eventos através de funções ~ Criamos um "click"
+const btnRemover = document.querySelector (".bnt3");
 
+//adicionando evento ao botão "adicionar"
 botaoAdd.addEventListener("click", function (evento) {
     evento.preventDefault();
 
     console.log('botao clicado');
 
-    // puxando um elemento do HTML, e criando uma variável em cima desse elemento
-
-    const inputUsuario = document.getElementById("novaTarefa");
-
-    //mensagem de erro caso o usuário não preencha o input
+    let inputUsuario = document.getElementById("novaTarefa");
 
     let mensagem = inputUsuario.value;
 
-    if (mensagem.trim() === "") {
+    let inputCategoria = document.getElementById("novaCategoria");
 
+    let etiqueta = inputCategoria.value;
+    //mensagem de erro caso você nao inserir o texto
+    if (mensagem.trim() === "" || etiqueta.trim() === "") {
+       
         erro.textContent = "Informe a nova tarefa";
 
     } else {
         erro.textContent = "";
-        // criando um elemento HTML através do JS e estilizando por aqui
 
         let novoItem = document.createElement("div");
         lista.appendChild(novoItem);
         novoItem.classList.add("novoItem");
 
-        // incluindo um elemento criado em divs que já existe no nosso HTML
-
         let tarefa = document.createElement("p");
-        tarefa.textContent = mensagem;
+        tarefa.textContent = mensagem; 
         novoItem.appendChild(tarefa);
         tarefa.classList.add("tarefaColor");
 
-        tarefa.addEventListener("click", function () {
-            if (tarefa.classList.contains("feito")) {
-                tarefa.classList.remove("feito")
-            } else {
-                tarefa.classList.add("feito");
-            }
-        })
-        // removendo item que o usuário criou
-
-        let xDesfazer = document.createElement("span");
-        xDesfazer.textContent = "x";
-        novoItem.appendChild(xDesfazer);
-        // puxando o item de remover e criando um click em cima do mesmo
-        xDesfazer.addEventListener("click", function () {
-            lista.removeChild(novoItem);
-        })
-
-        btnFeito.addEventListener("click", function (evento) {
-            evento.preventDefault();
-
-            if (tarefa.classList.contains("feito")) {
-                tarefa.classList.remove("feito")
-            } else {
-                tarefa.classList.add("feito");
-            }
-        })
+        // double click para deixar o conteudo editável
 
         tarefa.addEventListener("dblclick", function() {
             tarefa.classList.remove("feito")
             tarefa.setAttribute("contentEditable", true);
         })
+// evento para mandar o conteudo para a lista "invisivel"
+        tarefa.addEventListener("click", function () {
+            if (tarefa.classList.contains("feito")) { tarefa.classList.remove("feito") 
+        } else {
+                tarefa.classList.add("feito");
+            }
+        })
+        //dando vida a um novo botão
 
-        btnExcluir.addEventListener("click", function (evento) {
+        const novaEtiqueta = document.createElement("p");
+        novaEtiqueta.textContent = etiqueta;
+        novoItem.appendChild(novaEtiqueta);
+        novaEtiqueta.setAttribute("class", "etiqueta");
+
+
+        novaEtiqueta.addEventListener("dblclick", function() {
+            novaEtiqueta.setAttribute("contentEditable", true);
+        })
+
+
+
+//removendo item
+        let xDesfazer = document.createElement("span");
+        xDesfazer.textContent = "x";
+        novoItem.appendChild(xDesfazer);
+
+        xDesfazer.addEventListener("click", function () {
+            lista.removeChild(novoItem);
+        })
+
+        btnFeito.addEventListener ("click", function (evento) {
+            evento.preventDefault();
+
+            if(tarefa.classList.contains("feito")) {                
+                tarefa.classList.remove("feito")            
+            } else {
+                tarefa.classList.add("feito");
+            }
+        })
+      
+        btnExcluir.addEventListener("click", function(evento) {
             evento.preventDefault();
             lista.removeChild(novoItem);
         })
 
-        inputUsuario.value = "";
-
-        // Atributos para pegar item, arrastar item e recolocar item... "true" Valor Boleano...
+        btnRemover.addEventListener("click", function(evento) {
+            evento.preventDefault();
+            if (tarefa.classList.contains("feito")){
+                lista.removeChild(novoItem);
+            }            
+        })
+    //atributo para mover a lista aleatória mente
+    //lembrando que são 3 eventos diferentes
 
         tarefa.setAttribute("draggable", true);
         novoItem.setAttribute("draggable", true);
         xDesfazer.setAttribute("draggable", true);
-        // Criando uma variável e aplicando o dragstart, drangover e drangend para pegar, segurar e mover o item
+
         let dragging
 
         lista.addEventListener("dragstart", function (ev) {
             dragging = ev.target.closest(".novoItem");
 
         })
-
 
         lista.addEventListener("dragover", function (ev) {
             ev.preventDefault();
@@ -109,8 +123,7 @@ botaoAdd.addEventListener("click", function (evento) {
             dragging = null;
         })
     }
- 
+    inputUsuario.value = "";
 
-
+    inputUsuario.value = "";
 });
-
